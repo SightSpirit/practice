3c697af98b53575dfbb6c70e236ff6e8328ce0cf
SightSpirit <ezlospirit@gmail.com>
2017-10-23 21:52:02 -0400

    Update Chessboard.php

--- Chessboard.php
+++ Chessboard.php
@@ -8,7 +8,9 @@
 	$black = false;
 	
 	function color() {
-		return $black ? "black" : "white";
+		global $black;
+		if ($black) { return "black"; }
+		else { return "white"; }
 	}
 	
 	echo "<table style=\"width: 500px; height: 500px;\">";
@@ -16,6 +18,7 @@
 		echo "<tr>";
 		for ($j=1; $j<9; $j++) {
 			echo "<td style=\"width:12.5%; height:12.5%; background-color:".color().";\"></td>";
+			global $black;
 			if ($j!=8) $black = $black ? false : true;
 		}
 		echo "</tr>";
